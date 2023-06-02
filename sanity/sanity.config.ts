3b2b537bea36dd8d93d6729388f1bdaa4c208d22
Alex Puhl <alexpuhldeveloper@gmail.com>
2023-06-01 21:22:54 -0600

    First commit, started a Next and Sanity project

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,25 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+if (!process.env.SANITY_STUDIO_PROJECT_ID) {
+  throw new Error('SANITY_STUDIO_PROJECT_ID is not set')
+}
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'UtahJS-v3',
+
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
