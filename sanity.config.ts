a6be3d5f9fc7fe406e8dcc4993f21192ed9b07be
Alex Puhl <alexpuhldeveloper@gmail.com>
2023-06-24 01:16:58 -0600

    Created a env-config.ts file that gathers up env variables and sets fallbacks

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,16 +2,15 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { myStructure } from './sanity/deskStructure';
+import envConfig from './env-config';
 import schemas from './sanity/schemas';
 
-if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
-	throw new Error(`SANITY_STUDIO_PROJECT_ID is not set`);
-}
+const { SANITY_PROJECT_ID, SANITY_DATASET } = envConfig;
 
 export default defineConfig({
 	title: `UtahJS-v3`,
-	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-	dataset: `production`,
+	projectId: SANITY_PROJECT_ID,
+	dataset: SANITY_DATASET,
 	apiVersion: `2023-06-22`,
 	basePath: `/admin`,
 	plugins: [
